1a1a2b60afcf67eff755d8648a09ff9441daf440
Diederik <diederikjhattingh@gmail.com>
2023-08-12 20:14:47 +0000

    Fix await function

--- web/custom.js
+++ web/custom.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     fetchDataButton.addEventListener('click', async () => {
         try {
             const response = await fetch('https://diederik.builtwithdark.com/hello');
-            const data = response.text
+            const data = await response.text()
             contentElement.textContent = data;
         } catch (error) {
             console.error('Error fetching data:', error);
