06608c22eed70db341fed9b1e6b5c05539e55f6e
diederik <diederikjhattingh@gmail.com>
2023-08-13 10:42:33 +0200

    Calling gitlab endpoint. Cleanedup some JS response side code.

--- web/custom.js
+++ web/custom.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const contentElement = document.getElementById('commitp');
     const fetchDataButton = document.getElementById('button');
 
-    fetchDataButton.addEventListener('click', async () => {
-        try {
-            const response = await fetch('https://diederik.builtwithdark.com/hello');
-            const data = await response.text()
-            contentElement.textContent = data;
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    });
+    fetchDataButton.addEventListener('click', fetchData);
+
+    fetchData()
 });
+
+async function fetchData() {
+    const contentElement = document.getElementById('commitp');
+    try {
+      const urlRegex = /https?:\/\/[^\s/$.?#].[^\s]*/g;
+      const response = await fetch('https://diederik.builtwithdark.com/hello');
+      const data = await response.text();
+      contentElement.innerHTML = data.toString()
+        .replace(/^"|"$/g, '')    // Remove trailing and leading quotes
+        .replace(/\\n/g, '<br>')  // newlines to <br>
+        .replace(urlRegex, (url) => `<a href="${url}" target="_blank">${url}</a>`);  // Links
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
